053f41528cd6baf74c00e75b85da9bd7e8a7736c
Shreyash Gaikwad <shreyash.gaikwad@mindbowser.com>
2025-06-15 02:04:46 +0530

    added new code

--- week01/node/day01/arrys.js
+++ week01/node/day01/arrys.js
@@ -19,3 +19,20 @@ console.log(hobbies.map(hobby => {
 
 
 console.log(hobbies);
+
+
+const numbers = [-1,2,3,4,5];
+
+const two = numbers.find((value) => value === 2);
+
+console.log(two);
+
+
+const filteredValue = numbers.filter(num => num % 3 === 0);
+console.log(filteredValue);
+
+const sum = numbers.reduce((n1, n2) => n1 + n2, 3);
+console.log("Sum:", sum);
+
+const hasNegative = numbers.some(n => n < 0);
+console.log("Has negative?", hasNegative);
---
+++ week01/node/day01/nodePractice/hello.js
@@ -0,0 +1,3 @@
+console.log("hello welcome to learn node.js ");
+console.log("it provide runtime environment to js");
+// console.log("todays Date is",new Date());
