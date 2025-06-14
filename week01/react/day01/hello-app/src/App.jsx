e8e31e89036c54906975bba336bcc92f5ae19d16
Shreyash Gaikwad <shreyash.gaikwad@mindbowser.com>
2025-06-14 17:04:41 +0530

    updated the day 1 hello react file

--- week01/react/day01/hello-app/src/App.jsx
+++ week01/react/day01/hello-app/src/App.jsx
@@ -1,26 +1,31 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const myname = "Shreyash Gaikwad";
+    const age = 24;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-       <div className="hello">
-        <h1>Hello this is the first app of Vite+React</h1>
-      </div>
-    </>
-  )
+    <div className='hello-app'>
+      <header className='header'>
+        <h1>Hello Welcome to my React+Vite application</h1>
+        <h2>Current Year is : {new Date().getFullYear()}</h2>
+      </header>
+       <section className="profile">
+        <h2>About Me</h2>
+        <p>Hello! I'm <strong>{myname}</strong>, a new <b>MERN Stack</b> trainee developer at <strong>MindBowser</strong></p>
+      </section>
+      <section>
+          <label><strong>my Hobbies are :</strong></label>
+        <ol>
+          <li>Solving DSA</li>
+          <li>Learning CSS</li>
+        </ol>
+      </section>
+      <h3 style={{color:'blue'}}>motivational quote : Work hard until your signature becomes an autograph</h3>
+    </div>
+  );
 }
 
 export default App
